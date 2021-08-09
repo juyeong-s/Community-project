e1a3cac45f287e8b604f45d6e0d27171348c68e6
JuYeong <happyu9986@gmail.com>
2021-08-09 10:18:51 +0900

    username만 출력가능하게 변경

--- backend/community/views.py
+++ backend/community/views.py
@@ -7,15 +7,26 @@ from django.views.decorators.http import require_POST
 from .models import Post
 from .models import User
 
+def usersearch(postdata, userdata):
+    username = []
+    for post in postdata:
+        for user in userdata:
+            if post["writer_fk_id"] == user["id"]:
+                username.append(user["UserID"])
+    return username
+
+
 def getPostlist(request):
     # users = User.objects.all()
-    posts = Post.objects.all()
+    # posts = Post.objects.all()
     # user_list = []
     # post_list = []
     if request.method == 'GET':
         # users = list(User.objects.values())
-        posts = list(Post.objects.values())
-    postdata = list(posts)
+        postdata = list(Post.objects.values())
+        # userdata = list(User.objects.values('id','UserID'))
+        # username = usersearch(postdata,userdata)
+    # postdata = list(posts)
 
     return JsonResponse(postdata, safe=False)
 
@@ -23,11 +34,14 @@ def getUserlist(request):
     # users = User.objects.all()
     if request.method == 'GET':
         # users = list(User.objects.values())
-        users = list(User.objects.values())
+        postdata = list(Post.objects.values())
+        userdata = list(User.objects.values('id','UserID'))
+    # print(users)
 
-    userdata = list(users)
-
-    return JsonResponse(userdata, safe=False)
+    # userdata = list(users)
+    # print(userdata)
+    username = usersearch(postdata,userdata)
+    return JsonResponse(username, safe=False)
 
 @csrf_exempt
 @require_POST   # POST 메서드로 접근 시에만 동작
